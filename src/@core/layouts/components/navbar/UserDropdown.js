091bb04a5a9ff6e10817ade6ece1baa2a833e074
CalebHolt-15 <calebholt100@gmail.com>
2021-09-17 00:55:34 +0530

    Fetch userProfile

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -18,6 +18,7 @@ import { User, Mail, CheckSquare, MessageSquare, Settings, CreditCard, HelpCircl
 
 // ** Default Avatar Image
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg'
+import axios from "axios"
 
 const UserDropdown = () => {
   // ** Store Vars
@@ -25,59 +26,95 @@ const UserDropdown = () => {
 
   // ** State
   const [userData, setUserData] = useState(null)
+  const [fetched, setFetched] = useState(false)
 
   //** ComponentDidMount
   useEffect(() => {
     if (isUserLoggedIn() !== null) {
-      setUserData(JSON.parse(localStorage.getItem('userData')))
+      setUserData(JSON.parse(localStorage.getItem("userData")))
     }
   }, [])
 
   //** Vars
   const userAvatar = (userData && userData.avatar) || defaultAvatar
+  // *******   get user  ********
+  useEffect(() => {
+    console.log("getProfile")
+
+    const getProfile = async () => {
+      const options = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+        url: "https://127.0.0.1:8089/api/user"
+      }
+      try {
+        const { data } = await axios(options)
+        console.log("fetched profile", data)
+        setUserData(data)
+        setFetched(true)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    getProfile()
+  }, ["https://127.0.0.1:8089/api/user"])
 
   return (
-    <UncontrolledDropdown tag='li' className='dropdown-user nav-item'>
-      <DropdownToggle href='/' tag='a' className='nav-link dropdown-user-link' onClick={e => e.preventDefault()}>
-        <div className='user-nav d-sm-flex d-none'>
-          <span className='user-name font-weight-bold'>{(userData && userData['username']) || 'John Doe'}</span>
-          <span className='user-status'>{(userData && userData.role) || 'Admin'}</span>
+    <UncontrolledDropdown tag="li" className="dropdown-user nav-item">
+      <DropdownToggle
+        href="/"
+        tag="a"
+        className="nav-link dropdown-user-link"
+        onClick={(e) => e.preventDefault()}
+      >
+        <div className="user-nav d-sm-flex d-none">
+          <span className="user-name font-weight-bold">
+            {(userData && userData["username"]) || "John Doe"}
+          </span>
+          <span className="user-status">
+            {(userData && userData.role) || "Admin"}
+          </span>
         </div>
-        <Avatar img={userAvatar} imgHeight='40' imgWidth='40' status='online' />
+        <Avatar img={userAvatar} imgHeight="40" imgWidth="40" status="online" />
       </DropdownToggle>
       <DropdownMenu right>
-        <DropdownItem tag={Link} to='/pages/profile'>
-          <User size={14} className='mr-75' />
-          <span className='align-middle'>Profile</span>
+        <DropdownItem tag={Link} to="/pages/profile">
+          <User size={14} className="mr-75" />
+          <span className="align-middle">Profile</span>
         </DropdownItem>
-        <DropdownItem tag={Link} to='/apps/email'>
-          <Mail size={14} className='mr-75' />
-          <span className='align-middle'>Inbox</span>
+        <DropdownItem tag={Link} to="/apps/email">
+          <Mail size={14} className="mr-75" />
+          <span className="align-middle">Inbox</span>
         </DropdownItem>
-        <DropdownItem tag={Link} to='/apps/todo'>
-          <CheckSquare size={14} className='mr-75' />
-          <span className='align-middle'>Tasks</span>
+        <DropdownItem tag={Link} to="/apps/todo">
+          <CheckSquare size={14} className="mr-75" />
+          <span className="align-middle">Tasks</span>
         </DropdownItem>
-        <DropdownItem tag={Link} to='/apps/chat'>
-          <MessageSquare size={14} className='mr-75' />
-          <span className='align-middle'>Chats</span>
+        <DropdownItem tag={Link} to="/apps/chat">
+          <MessageSquare size={14} className="mr-75" />
+          <span className="align-middle">Chats</span>
         </DropdownItem>
         <DropdownItem divider />
-        <DropdownItem tag={Link} to='/pages/account-settings'>
-          <Settings size={14} className='mr-75' />
-          <span className='align-middle'>Settings</span>
+        <DropdownItem tag={Link} to="/pages/account-settings">
+          <Settings size={14} className="mr-75" />
+          <span className="align-middle">Settings</span>
         </DropdownItem>
-        <DropdownItem tag={Link} to='/pages/pricing'>
-          <CreditCard size={14} className='mr-75' />
-          <span className='align-middle'>Pricing</span>
+        <DropdownItem tag={Link} to="/pages/pricing">
+          <CreditCard size={14} className="mr-75" />
+          <span className="align-middle">Pricing</span>
         </DropdownItem>
-        <DropdownItem tag={Link} to='/pages/faq'>
-          <HelpCircle size={14} className='mr-75' />
-          <span className='align-middle'>FAQ</span>
+        <DropdownItem tag={Link} to="/pages/faq">
+          <HelpCircle size={14} className="mr-75" />
+          <span className="align-middle">FAQ</span>
         </DropdownItem>
-        <DropdownItem tag={Link} to='/login' onClick={() => dispatch(handleLogout())}>
-          <Power size={14} className='mr-75' />
-          <span className='align-middle'>Logout</span>
+        <DropdownItem
+          tag={Link}
+          to="/login"
+          onClick={() => dispatch(handleLogout())}
+        >
+          <Power size={14} className="mr-75" />
+          <span className="align-middle">Logout</span>
         </DropdownItem>
       </DropdownMenu>
     </UncontrolledDropdown>
